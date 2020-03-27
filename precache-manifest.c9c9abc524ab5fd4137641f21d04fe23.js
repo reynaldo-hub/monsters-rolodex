9fbe096323be4d872300b86faab82820232726c2
Reynaldo Rodriguez <rodriguezreynaldo1698@outlook.com>
2020-03-26 22:00:55 -0600

    Updates

---
+++ precache-manifest.c9c9abc524ab5fd4137641f21d04fe23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f199afe43657846f0d36f516a38463",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "162f70738a74b83aec3e",
+    "url": "/monster-rolodex/static/css/main.c7d2d3bf.chunk.css"
+  },
+  {
+    "revision": "23896a2b4ff8127a7a67",
+    "url": "/monster-rolodex/static/js/2.10c9861d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.10c9861d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162f70738a74b83aec3e",
+    "url": "/monster-rolodex/static/js/main.b2275393.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
